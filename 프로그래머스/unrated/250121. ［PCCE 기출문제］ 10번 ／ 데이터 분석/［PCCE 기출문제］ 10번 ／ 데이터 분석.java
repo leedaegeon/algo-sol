6a6e9ae53d55bde8cf1d76394debde5efc821614
leedaegeon <48538519+leedaegeon@users.noreply.github.com>
2024-01-11 14:40:08 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 2.96 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -4,17 +4,24 @@ class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
         int[][] answer = {};
         int[][] tempAnswer = new int[data.length][4];
+        String[] cmp = {"code", "date", "maximum", "remain"};
         
         int idx = 0;
-        if(ext.equals("code")){
-            idx = 0;
-        }else if(ext.equals("date")){
-            idx = 1;
-        }else if(ext.equals("maximum")){
-            idx = 2;
-        }else if(ext.equals("remain")){
-            idx = 3;
+        
+        for(int i=0; i<4; i++){
+            if(ext.equals(cmp[i])){
+                idx = i;
+                break;
+            }
         }
+        int sortIdx = 0;
+        for(int i=0; i<4; i++){
+            if(sort_by.equals(cmp[i])){
+                sortIdx = i;
+                break;
+            }
+        }
+        
         int ansIdx=0;
         for(int i=0; i<data.length; i++){
             if(data[i][idx] < val_ext){
@@ -25,24 +32,19 @@ class Solution {
         for(int i=0; i<ansIdx; i++){
             answer[i] = tempAnswer[i];
         }
+
         
-        if(sort_by.equals("code")){
-            Arrays.sort(answer, (o1, o2)->{return o1[0]-o2[0];});
-            
-        }else if(sort_by.equals("date")){
-            Arrays.sort(answer, (o1, o2)->{return o1[1]-o2[1];});
-        }else if(sort_by.equals("maximum")){
-            Arrays.sort(answer, (o1, o2)->{return o1[2]-o2[2];});
-            
-        }else if(sort_by.equals("remain")){
-            Arrays.sort(answer, (o1, o2)->{return o1[3]-o2[3];});
-        }
-        for(int i=0; i<answer.length; i++){
-            for(int j=0; j<4; j++){
-                System.out.print(tempAnswer[i][j] + " ");
-            }
-            System.out.println();
-        }
+        final int finalSortIdx = sortIdx;
+        
+        Arrays.sort(answer, (o1, o2)->{
+                    return o1[finalSortIdx] - o2[finalSortIdx];
+                });
+        // for(int i=0; i<answer.length; i++){
+        //     for(int j=0; j<4; j++){
+        //         System.out.print(tempAnswer[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
         return answer;
     }
 }
