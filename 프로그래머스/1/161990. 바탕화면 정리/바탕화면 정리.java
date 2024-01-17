990cf1165e0c875a322682c1d8c2a083094c1cce
이대건 <ldg03198@gmail.com>
2024-01-17 20:06:04 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.21 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -2,31 +2,28 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] wallpaper) {
         int[] answer = new int[4];
-        List<Integer> rowList = new LinkedList<>();
-        List<Integer> colList = new LinkedList<>();
+        int lux = Integer.MAX_VALUE;
+        int luy = Integer.MAX_VALUE;
+        int rdx = Integer.MIN_VALUE;
+        int rdy = Integer.MIN_VALUE;
         
         for(int i=0; i<wallpaper.length; i++){
             char[] row = wallpaper[i].toCharArray();
             for(int j=0; j<wallpaper[i].length(); j++){
                 if(row[j] == '#'){
-                    rowList.add(i);
-                    colList.add(j);
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i);
+                    rdy = Math.max(rdy, j);
                 }    
             }
         }
-        Collections.sort(rowList);
-        Collections.sort(colList);
-        answer[0] = rowList.get(0);
-        answer[1] = colList.get(0);
-        answer[2] = rowList.get(rowList.size()-1)+1;
-        answer[3] = colList.get(colList.size()-1)+1;
-        // for(int item: rowList){
-        //     System.out.print(item + " ");
-        // }
-        // System.out.println("\n==================");
-        // for(int item: colList){
-        //     System.out.print(item + " ");
-        // }
+        
+        answer[0] = lux;
+        answer[1] = luy;
+        answer[2] = rdx+1;
+        answer[3] = rdy+1;
+        
         
         
         return answer;
