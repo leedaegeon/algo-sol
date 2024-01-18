0cdc5eacefe548cb554a691cb20be5609da99844
이대건 <ldg03198@gmail.com>
2024-01-18 22:29:59 +0900

    [level 2] Title: 숫자 변환하기, Time: 14.97 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -6,30 +6,25 @@ class Solution {
         
         int[] dp = new int[y+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[x] = 0;
+        dp[y] = 0;
         
-        for(int i = x; i <= y; i++){
+        for(int i = y; i>= x; i--){
             if(dp[i] == Integer.MAX_VALUE){
                 continue;
             }
-            if(i+n <= y){
-                dp[i+n] = Math.min(dp[i] + 1, dp[i+n]);
+            if(i-n>=x){
+                dp[i-n] = Math.min(dp[i] + 1, dp[i-n]);
             }
-            if(i*2 <= y){
-                dp[i*2] = Math.min(dp[i] + 1, dp[i*2]);
+            if(i%2==0){
+                dp[i/2] = Math.min(dp[i] + 1, dp[i/2]);
             }
-            if(i*3 <= y){
-                dp[i*3] = Math.min(dp[i]+1, dp[i*3]);
+            if(i%3 == 0){
+                dp[i/3] = Math.min(dp[i] + 1, dp[i/3]);
             }
+            
         }
-        // for(int i=x; i<=y; i++){
-        //     if(dp[i] == Integer.MAX_VALUE){
-        //         continue;
-        //     }
-        //     System.out.println(dp[i]);
-        // }
         
         
-        return dp[y]==Integer.MAX_VALUE?-1:dp[y];
+        return dp[x]==Integer.MAX_VALUE?-1:dp[x];
     }
 }
