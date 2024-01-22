d0adf48a671e19a6d13c35ff1d85897e9b816ed5
이대건 <ldg03198@gmail.com>
2024-01-22 22:31:49 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1414.00 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -5,32 +5,52 @@ class Solution {
         Set<Integer> hs = new HashSet<>();
         Deque<Integer> dq = new ArrayDeque<>();
         int last = 0;
-        for(int i=0; i<elements.length; i++){
-            dq.offerLast(elements[i]);
-            last += elements[i];
-            hs.add(elements[i]);
-        }
-        hs.add(last);
+        // for(int i=0; i<elements.length; i++){
+        //     dq.offerLast(elements[i]);
+        //     last += elements[i];
+        //     hs.add(elements[i]);
+        // }
+        // hs.add(last);
         
-        int temp = 0;
-//         i개씩 뽑아보는 것을 j번
-        for(int i=2; i<elements.length; i++){
+        // int temp = 0;
+//         for(int j=0; j<elements.length; j++){
+
+//             for(int i=1; i<elements.length-1; i++){
+//                 int sum = 0;
+//                 for(int k=0; k<=i; k++){
+//                     temp = dq.poll();
+//                     sum+=temp;
+//                     // System.out.print(temp + " ");
+//                     dq.offerLast(temp);
+                    
+//                 }
+//                 hs.add(sum);
+//                 // System.out.println();
+//                 // if(hs.add(sum)){
+//                 //     System.out.print(sum + " ");
+//                 // }
+//             }
+//             // System.out.println();
+//         }
+        
+        for(int i=0; i<elements.length; i++){
             for(int j=0; j<elements.length; j++){
                 int sum = 0;
-                for(int k=0; k<i; k++){
-                    temp = dq.pollFirst();
-                    System.out.print(temp + " ");
-                    sum += temp;
-                    dq.offerLast(temp);
+                for(int k=0; k<=i; k++){
+                    
+                    if(j+k >= elements.length){
+                        sum+=elements[(j+k)%elements.length];
+                    }else{
+                        sum += elements[j+k];
+                    }
                 }
                 hs.add(sum);
                 // if(hs.add(sum)){
                 //     System.out.print(sum + " ");
                 // }
-                System.out.println();
-                
             }
             // System.out.println();
+
         }
         answer = hs.size();
         
