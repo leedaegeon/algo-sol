9a1255db6876367bfd96daa45349ce7f700d2667
이대건 <ldg03198@gmail.com>
2024-01-31 11:23:13 +0900

    [level 2] Title: 롤케이크 자르기, Time: 272.40 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,26 +1,32 @@
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
-        int answer = 0;
-        
-        int[] bigArr = new int[10_001];
-        
+         int answer = 0;
+        Map<Integer, Integer> big = new HashMap<>();
+        Map<Integer, Integer> small = new HashMap<>();
         Set <Integer> bigSet = new HashSet<>();
         Set<Integer> smallSet = new HashSet<>();
-        
-        for(int t:topping){
-            bigArr[t] += 1;
-            bigSet.add(t);
+
+        for(int t: topping){
+            if(big.get(t) == null){
+                big.put(t, 1);
+                bigSet.add(t);
+            }else{
+                big.put(t, big.get(t) + 1);
+            }
         }
-        
         for(int t:topping){
-            bigArr[t] -= 1;
-            
-            if(bigArr[t] == 0){
+            big.put(t, big.get(t) -1);
+            if(big.get(t).equals(0)){
                 bigSet.remove(t);
             }
-            smallSet.add(t);
-            
+
+            if(small.get(t) == null){
+                small.put(t, 1);
+                smallSet.add(t);
+            }else{
+                small.put(t, small.get(t) + 1);
+            }
             if(bigSet.size() == smallSet.size()){
                 answer++;
             }
@@ -28,7 +34,7 @@ class Solution {
         // for(Map.Entry e: big.entrySet()){
         //     System.out.print(e.getKey() + " " + e.getValue() + " \n");
         // }
-        
+
         return answer;
     }
 }
