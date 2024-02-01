0478ad65e2733d319f823b1ef0b5c21090daebc5
이대건 <ldg03198@gmail.com>
2024-02-01 15:33:03 +0900

    [level 2] Title: 프로세스, Time: 0.54 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        int[] p = priorities;
+
+        Integer[] sorted = new Integer[priorities.length];
+        Queue<Integer> q = new ArrayDeque<>();
+        
+        for(int i=0; i<priorities.length; i++){
+            sorted[i] = p[i];    
+            q.offer(p[i]);
+        }
+        Arrays.sort(sorted, Collections.reverseOrder());
+        int idx = 0;
+        while(!q.isEmpty()){
+            int current = q.poll();
+            if(current != sorted[idx]){
+                q.offer(current);                
+                if(location == 0){
+                location = q.size()-1;
+                }else{
+                    location--;
+                }
+                continue;
+            }
+            answer++;
+            if(location == 0){
+                break;
+            }
+            idx++;
+            location--;
+            // System.out.println(current + " " + location);
+            
+        }
+            
+        
+        return answer;
+    }
+}
