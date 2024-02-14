1e6374cc16aae86edc512bc190fae95ffb98c256
이대건 <ldg03198@gmail.com>
2024-02-14 15:23:35 +0900

    [level 2] Title: 택배상자, Time: 82.92 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,48 +2,22 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        Queue<Integer> mainQ = new LinkedList<>();
         Deque<Integer> subStack = new LinkedList<>();
-        for(int i=1; i<=order.length; i++){
-            mainQ.add(i);
-        }
+        int idx = 0;
         for(int i=0; i<order.length; i++){
-            // System.out.println(order[i] + " " + answer);
+            subStack.addFirst(i+1);
             
-            if(!mainQ.isEmpty() && order[i] == mainQ.peek()){
-
-                mainQ.poll();
-                answer++;
-                continue;
-            }
-            if(!subStack.isEmpty() && order[i] == subStack.peekFirst()){
-                subStack.removeFirst();
-                answer++;
-                continue;
-            }
-            
-            while(!mainQ.isEmpty()){
-                int val = mainQ.poll();
-                if(order[i] > val){
-                    subStack.addFirst(val);    
-                }else if(order[i] == val){
+            while(!subStack.isEmpty()){
+                int val  = subStack.peekFirst();
+                if(val == order[idx]){
+                    subStack.removeFirst();
                     answer++;
-                    break;
+                    idx++;
                 }else{
-                    return answer;
+                    break;
+
                 }
-                
             }
-            
-            
-            // for(int a: mainQ){
-            //     System.out.print(a + " ");
-            // }
-            // System.out.println("\n========================");
-            // for(int a: subStack){
-            //     System.out.print(a + " ");
-            // }           
-            // System.out.println("\n--------------------------");
 
         }
         return answer;
