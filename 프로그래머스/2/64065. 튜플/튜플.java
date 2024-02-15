7bd08743c427985beafc1f896f01472f33f8eecd
이대건 <ldg03198@gmail.com>
2024-02-15 15:27:50 +0900

    [level 2] Title: 튜플, Time: 0.70 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,46 +2,22 @@ import java.io.*;
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-        String s1 = s.substring(1, s.length()-1);
-        // System.out.println(s1);
-        StringTokenizer st = new StringTokenizer(s1, "{}");
+        int[] answer = {};
+        
+        String[] strArr = s.replaceAll("[{}]", " ").trim().split(" , ");
         Set<String> hs = new HashSet<>();
-        List<List<String>> ls = new LinkedList<>();
-        for(int i=0; i<st.countTokens()+1; i++){
-            ls.add(new LinkedList<>());
-        }
-        int arrSize = 0;
-        while(st.hasMoreTokens()){
-            
-            // hs.add(st.nextToken());
-            String next = st.nextToken();
-            if(next.equals(",")){
-                continue;
-            }
-            StringTokenizer subSt = new StringTokenizer(next, ",");
-            int size = subSt.countTokens();
-            while(subSt.hasMoreTokens()){
-                String subNext = subSt.nextToken();
-                if(subNext.equals(",")){
-                    continue;
-                }
-                ls.get(size).add(subNext);
-            }
-            
-            // System.out.println(next.length());
-            arrSize++;
-        }
-        int[] answer = new int[arrSize];
+        
+        Arrays.sort(strArr, (o1, o2) -> {
+            return o1.length() - o2.length();
+        });
+        answer = new int[strArr.length];
         int idx = 0;
-        for(List<String> a: ls){
-            for(String x : a){
-                if(hs.add(x)){
-                    answer[idx] = Integer.parseInt(x);
-                    idx++;
-                }
-                // System.out.print(x + " ");
+        for(String s2 : strArr){
+            for(String s3 : s2.split(",")){
+                if(hs.add(s3)){
+                    answer[idx++] = Integer.parseInt(s3);
+                }        
             }
-            // System.out.println();
         }
         
         return answer;
