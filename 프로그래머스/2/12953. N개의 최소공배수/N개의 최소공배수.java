46a034fde36af0b4df68e50c2ba83e695c018f4e
이대건 <ldg03198@gmail.com>
2024-02-16 13:41:52 +0900

    [level 2] Title: N개의 최소공배수, Time: 2329.81 ms, Memory: 496 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -3,37 +3,30 @@ class Solution {
     public int solution(int[] arr) {
         int answer = 0;
         
-        Arrays.sort(arr);
         Map<Integer, Integer> map = new HashMap<>();
         int[] nextArr = new int[arr.length];
         for(int i=0; i<arr.length; i++){
             nextArr[i] = arr[i];
         }
-        int idx = 0;
+        int idx = -1;
         
         while(true){
-            
+            idx++;
+            idx %= arr.length;
             int next = nextArr[idx];
             if(map.get(next) == null){
                 map.put(next, 1);
                 nextArr[idx] += arr[idx];
-                idx++;
-                idx %= arr.length;
+                
                 continue;
             }
             map.put(next, map.get(next)+1);
             nextArr[idx] += arr[idx];
-            // for(Map.Entry m: map.entrySet()){
-            //     System.out.println(m.getKey() + " "+ m.getValue());
-            // }
-            // System.out.println("====================");
+
             if(map.get(next) == arr.length){
                 answer = next;
                 break;
             }
-            
-            idx++;
-            idx %= arr.length;
             
         }
 
