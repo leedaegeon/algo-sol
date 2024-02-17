ec414850f09a350fb6466383e04531e1de0c60c8
이대건 <ldg03198@gmail.com>
2024-02-17 16:46:46 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.31 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,74 +1,37 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        Map<Integer, Integer> minMap = new TreeMap<>();
-        Map<Integer, Integer> maxMap = new TreeMap<>(Collections.reverseOrder());
-
+        int[] answer = {};
+        TreeMap<Integer, Integer> tmap = new TreeMap<>();
         for(int i=0; i<operations.length; i++){
-            String[] strArr = operations[i].split(" ");
-            // System.out.print(strArr[0] + " " + strArr[1] + " ");
-            if(strArr[0].equals("I")){
-                // System.out.println();
-                int val = Integer.parseInt(strArr[1]);
-                minMap.put(val, minMap.getOrDefault(val, 0) + 1);
-                maxMap.put(val, maxMap.getOrDefault(val, 0) + 1);
-            }
-            else if(strArr[1].equals("-1")){
-                if(minMap.keySet().size() <= 0){
-                    continue;
-                }
-                int minVal = minMap.keySet().iterator().next();
-                // System.out.println(minVal);
-                if(minMap.get(minVal) != null && maxMap.get(minVal) != null && minMap.get(minVal) <= 1){
-                    minMap.remove(minVal);
-                    maxMap.remove(minVal);
-                }else{
-                    minMap.put(minVal, minMap.get(minVal) - 1);
-                    maxMap.put(minVal, maxMap.get(minVal) - 1);
+            String[] op = operations[i].split(" ");
+            if(op[0].equals("I")){
+                int val = Integer.parseInt(op[1]);
+                tmap.put(val, tmap.getOrDefault(val, 0) + 1);
+            }else if(op[1].equals("-1")){
+                if(tmap.size() > 0){
+                    int minVal = tmap.firstKey();
+                    tmap.put(minVal, tmap.get(minVal) - 1);
+                    if(tmap.get(minVal) == 0){
+                        tmap.remove(minVal);
+                    }
                 }
             }else{
-                if(maxMap.keySet().size() <= 0){
-                    continue;
-                }
-                int maxVal = maxMap.keySet().iterator().next();
-                // System.out.println(maxVal);
-                if(minMap.get(maxVal) != null && maxMap.get(maxVal) != null && maxMap.get(maxVal) <= 0){
-                    minMap.remove(maxVal);
-                    maxMap.remove(maxVal);
-                }else{
-                    minMap.put(maxVal, minMap.get(maxVal) - 1);
-                    maxMap.put(maxVal, maxMap.get(maxVal) - 1);
+                
+                if(tmap.size() > 0){
+                    int maxVal = tmap.lastKey();
+                    tmap.put(maxVal, tmap.get(maxVal) - 1);
+                    if(tmap.get(maxVal) == 0){
+                        tmap.remove(maxVal);
+                    }
                 }
             }
-            // System.out.print("minMap: ");
-            // for(Map.Entry<Integer, Integer> m: minMap.entrySet()){
-            //     System.out.print(m + ", ");
-            // }
-            // System.out.println();
-            // System.out.print("maxMap: ");
-            // for(Map.Entry<Integer, Integer> m: maxMap.entrySet()){
-            //     System.out.print(m + ", ");
-            // }
-            // System.out.println("\n====================");
         }
-        int minVal = 0;
-        int maxVal = 0;
-        
-        for(Map.Entry<Integer, Integer> m : minMap.entrySet()){
-            if((int)m.getValue() > 0){
-                minVal = m.getKey();
-                break;
-            }
+        answer = new int[2];
+        if(tmap.size() > 0){
+            answer[0] = tmap.lastKey();
+            answer[1] = tmap.firstKey();
         }
-        for(Map.Entry<Integer, Integer> m : maxMap.entrySet()){
-            if((int)m.getValue() > 0){
-                maxVal = m.getKey();
-                break;
-            }
-        }     
-        answer[0] = maxVal;
-        answer[1] = minVal;
         return answer;
     }
 }
