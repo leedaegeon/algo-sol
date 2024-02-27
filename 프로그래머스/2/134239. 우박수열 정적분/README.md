678f5eed04279719fc26eb416200c0227176262b
이대건 <ldg03198@gmail.com>
2024-02-27 12:24:35 +0900

    [level 2] Title: 우박수열 정적분, Time: 3.05 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/README.md
+++ 프로그래머스/2/134239. 우박수열 정적분/README.md
@@ -1,10 +1,10 @@
 # [level 2] 우박수열 정적분 - 134239 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134239#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134239) 
 
 ### 성능 요약
 
-메모리: 89.8 MB, 시간: 4.05 ms
+메모리: 84.7 MB, 시간: 3.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 27일 12:13:28
+2024년 02월 27일 12:24:34
 
 ### 문제 설명
 
--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -13,39 +13,28 @@ class Solution {
                 k+=1;
             }
         }
-        double[] integral = new double[ls.size()];
-        double[] sum = new double[ls.size()];
+        ls.add(1);
+
+        double[] sum = new double[ls.size()-1];
+        int max = ls.size()-1;
         
-        int max = ls.size();
-        for(int i=0; i<ls.size(); i++){
-            if(i < ls.size()-1){
-                // integral[i] =  (double)(ls.get(i) + ls.get(i+1))/2;
-                sum[i] = (double)(ls.get(i) + ls.get(i+1))/2;
-            }else{
-                // integral[i] = (double)(ls.get(i) + 1)/2;
-                sum[i] = (double)(ls.get(i) + 1)/2;
-            }
-            // if(i!=0){
-            //     integral[i] += integral[i-1];
-            // }
+        
+        for(int i=0; i<ls.size()-1; i++){
+            sum[i] = (double)(ls.get(i) + ls.get(i+1))/2;
         }
+        
         for(int i=0; i<ranges.length; i++){
             ranges[i][1] = max + ranges[i][1];
-            if(ranges[i][0] > ranges[i][1]){
+            if(ranges[i][1] < ranges[i][0]){
                 answer[i] = -1.0;
                 continue;
             }
-            // if(ranges[i][0] == 0){
-            //     answer[i] = integral[ranges[i][1]-1];
-            // }else{
-            //      answer[i] = integral[ranges[i][1]-1] - integral[ranges[i][0]-1];
-            // }
             for(int start = ranges[i][0]; start < ranges[i][1]; start++){
-                answer[i] += sum[start];
+                answer[i] += (double)sum[start];
             }
+        
             
         }
-        
         
         return answer;
     }
