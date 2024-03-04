792adf1d77613dec596c458f37883fd30e3b5b09
이대건 <ldg03198@gmail.com>
2024-03-04 15:23:42 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.65 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,61 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        PriorityQueue<Process> pq = new PriorityQueue<>();
+        
+        
+
+        Arrays.sort(jobs, (o1, o2) -> {
+            if(o1[0] == o2[0]){
+                return o1[1] - o2[1];
+            }else{
+                return o1[0] - o2[0];
+            }
+        });
+    
+        boolean[] visited = new boolean[jobs.length];
+
+        int t = 0;
+        int time = 0;
+        int idx = 0;
+        while(idx<jobs.length || !pq.isEmpty()){
+            
+            for(int i=idx; i<jobs.length; i++){
+                if(jobs[i][0] <= t){
+                    // System.out.println(jobs[i][0] + " " + jobs[i][1]);
+                    pq.offer(new Process(jobs[i][0], jobs[i][1]));
+                    idx++;
+                }
+                
+            }
+            if(!pq.isEmpty()){
+                Process p = pq.poll();
+                time += t - p.start + p.duration;
+                t += p.duration;
+            }else{
+                t++;
+            }
+            // System.out.println(t);
+        
+        }
+        
+        answer = (int)Math.floor(time / jobs.length);
+        return answer;
+    }
+    
+    class Process implements Comparable<Process>{
+        int start;
+        int duration;
+        
+        Process( int start, int duration){
+            this.start = start;
+            this.duration = duration;
+        }
+        public int compareTo(Process o2){
+            
+            return this.duration - o2.duration;
+            
+        }
+    }
+}
