ff680b87a8447528cd29aace01f0e71525f37024
이대건 <ldg03198@gmail.com>
2024-03-06 11:42:00 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,54 +4,54 @@ class Solution {
         int answer = 0;
         Map<String, Integer> map = new HashMap<>();
         Queue<String> q = new ArrayDeque<>();
-        
+        int total = 0;
+        for(int a: number){
+            total += a;
+        }
         for(int i=0; i<number.length; i++){
             map.put(want[i], number[i]);
         }
         for(int i=0; i<10; i++){
             if(map.get(discount[i]) != null){
                 map.put(discount[i], map.get(discount[i]) -1);
+                if(map.get(discount[i]) >= 0){
+                    total--;
+                }
             }
             q.offer(discount[i]);
         }
         
-        int cnt = 0;
-        for(Map.Entry<String, Integer> e: map.entrySet()){
-            // System.out.println(e.getKey() + " " + e.getValue());
-            if(e.getValue()<=0){
-                cnt++;
-            }
-        }
-        if(cnt == want.length){
+        if(total == 0){
             answer++;
         }
         int next = 10;
         
         while(next < discount.length || !q.isEmpty()){
             String item = q.poll();
-            cnt = 0;
             
             if(map.get(item) != null){
                 map.put(item, map.get(item) + 1);
+                if(map.get(item) > 0){
+                    total++;
+                }
             }
-            
+            if(total == 0){
+                answer++;
+            }
             if(next < discount.length){
                 String nextItem = discount[next++];
                 if(map.get(nextItem) != null){
                     map.put(nextItem, map.get(nextItem)-1);
+                    if(map.get(nextItem) >= 0){
+                        total--;
+                    }
                 }
                 q.offer(nextItem);
             }
-            
-            for(Map.Entry<String, Integer> e: map.entrySet()){
-            // System.out.println(e.getKey() + " " + e.getValue());
-                if(e.getValue()<=0){
-                    cnt++;
-                }
-            }
-            if(cnt == want.length){
+            if(total == 0){
                 answer++;
             }
+            
         }
         
         return answer;
