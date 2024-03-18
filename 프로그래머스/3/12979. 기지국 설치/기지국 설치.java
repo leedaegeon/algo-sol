23f0776b53815f056764eff0ae6ce60834afa2fb
이대건 <ldg03198@gmail.com>
2024-03-18 11:36:52 +0900

    [level 3] Title: 기지국 설치, Time: 1.05 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,23 +1,19 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int now = 0;
-        int i = 1;
-        while(i <= n){
-            // if(now < stations.length && i < stations[now] - w){
-            //     answer++;
-            //     i += w * 2 + 1;
-            // }
-            // else if(now<stations.length && i>= stations[now]-w){
-            //     i = stations[now] + w + 1;
-            //     now++;
-            // }
-            if(now < stations.length && i >= stations[now] - w){
-                i = stations[now++] + w + 1;
-            }
-            else{
+        int now = 1;
+        int idx = 0;
+        while(now <= n){
+            if(idx<stations.length 
+               && now < stations[idx]-w){
                 answer++;
-                i += w * 2 + 1;
+                now += 2*w+1;  
+            }else if(idx < stations.length){
+                now = stations[idx] + w + 1;    
+                idx++;
+            }else {
+                answer++;
+                now += 2*w+1;
             }
         }
         return answer;
