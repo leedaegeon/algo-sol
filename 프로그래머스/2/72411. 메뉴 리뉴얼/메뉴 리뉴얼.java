af99e6a5add8c6c775fd1717de46db3f322a3e69
이대건 <ldg03198@gmail.com>
2024-03-22 18:07:49 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 15.39 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Solution {
-    static Map<String, Integer> map = new HashMap<>();
+    static TreeMap<String, Integer> map = new TreeMap<>();
     static Map<Integer, Integer> max = new HashMap<>();
     
     public List<String> solution(String[] orders, int[] course) {
@@ -16,16 +16,14 @@ class Solution {
                 comb(0, 0, cards, new TreeSet<>(), course[j]);
             }
         }
-        // for(Map.Entry<String, Integer> e: map.entrySet()){
-        //     System.out.println(e.getKey() + " " + e.getValue());
-        // }
+        
         List<String> answer = new ArrayList<>();
         for(Map.Entry<String, Integer> e: map.entrySet()){
             if(e.getValue()>=2 && max.get(e.getKey().length()) == e.getValue()){
                 answer.add(e.getKey());
             }
         }
-        Collections.sort(answer);
+        // Collections.sort(answer);
         return answer;
     }
     public void comb(int idx, int cnt, List<Character> cards, TreeSet<Character> ts, int n){
