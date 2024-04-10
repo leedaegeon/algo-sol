170b15820735808e4eb255750df6e34500bfcd56
이대건 <ldg03198@gmail.com>
2024-04-11 01:58:40 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,34 +2,34 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 0;
-        int temp = n;
-        while(true){
-            if(temp==1){
-                break;
-            }
-            temp=temp>>1;
-            answer++;
-        }
+//         최대 경기수
+        int answer = Integer.toBinaryString(n).length()-1;
+        
         // System.out.println(Double.compare(2.0, 1.0));
         double min = Math.min(a,b);
         double max = Math.max(a,b);
-        double start = 1;
-        double end = n;
-        // System.out.println(answer);
+        
+//         이진탐색으로 두 참가자가 서로 어느 위치만큼 떨어져있는지 탐색
+        double start = 1.0;
+        double end = n*1.0;        
         while(true){
-            
+//             탐색할 지점
             double mid = start + (end-start)/2.0;
-            // System.out.println(mid + " " + min + " " + max);
+            
+//             double 타입은 Double.compare 사용하기
+//             min<=mid
             boolean leftFlag = Double.compare(min, mid) != 1?true:false;
+//             mid <= max
             boolean rightFlag = Double.compare(mid, max) != 1?true:false;
-            boolean rightFlagRev = Double.compare(mid, max) != -1?true:false;
+            
+//             min <= mid && max >= mid
             if(leftFlag && rightFlag){
                 break;
-
-            }else if(leftFlag && rightFlagRev){
+//              min < mid && max < mid       
+            }else if(leftFlag && !rightFlag){
                 end = mid;
             }else{
+//              min > mid && max > mid
                 start = mid;
             }
             answer--;
