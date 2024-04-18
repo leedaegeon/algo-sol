ba017fb809910cc1f7d2531264653f57c2078662
이대건 <ldg03198@gmail.com>
2024-04-18 14:06:14 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 264 ms, Memory: 29988 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
@@ -9,26 +10,26 @@ public class Main {
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
         int k = Integer.parseInt(input[1]);
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(n, 0));
+        Deque<Node> dq = new LinkedList<>();
+        dq.offer(new Node(n, 0));
         int answer = Integer.MAX_VALUE;
         Set<Integer> set = new HashSet<>();
-        while (!q.isEmpty()) {
-            Node now = q.poll();
+        while (!dq.isEmpty()) {
+            Node now = dq.pollFirst();
+//            System.out.println(now.number);
             if (now.number == k) {
-                answer = Math.min(answer, now.cnt);
-            }
-            if(now.number>0 && now.number<100_000 && set.add(now.number*2)) {
-                q.offer(new Node(now.number * 2, now.cnt));
+                answer = now.cnt;
+                break;
             }
             if(now.number > 0 && set.add(now.number-1)) {
-                q.offer(new Node(now.number - 1, now.cnt + 1));
+                dq.addLast(new Node(now.number - 1, now.cnt + 1));
+            }
+            if(now.number>0 && now.number<k && set.add(now.number*2)) {
+                dq.addFirst(new Node(now.number * 2, now.cnt));
             }
             if(now.number <= k && set.add(now.number+1)){
-                q.offer(new Node(now.number + 1, now.cnt + 1));
+                dq.addLast(new Node(now.number + 1, now.cnt + 1));
             }
-
-
         }
         System.out.println(answer);
     }
