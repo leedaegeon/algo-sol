626bf96fc7fd607d5e8b7081f722d47055c6342a
이대건 <ldg03198@gmail.com>
2024-04-27 22:22:31 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 7.10 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -4,10 +4,13 @@ class Solution {
         for(int i=0; i<numbers.length; i++){
             long k = 1;
             while(true){
-                long findZero = numbers[i] | k;
-                if(findZero > numbers[i]){
+                long findLowestZeroBit = numbers[i] | k;
+                if(findLowestZeroBit > numbers[i]){
+                    // 최하위 0비트의 이전비트는 1이므로 0으로 변환하기 위해 
+                    // 기존 k를 오른쪽 1비트 쉬프트
+                    // 후 XOR로 1을 0으로 토글
                     k >>= 1;
-                    answer[i] = findZero^k;
+                    answer[i] = findLowestZeroBit^k;
                     break;
                 }else{
                     k <<= 1;
