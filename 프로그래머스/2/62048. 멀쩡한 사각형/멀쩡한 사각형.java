cdbe50e10de91685e637bf2540692516c35730fa
이대건 <ldg03198@gmail.com>
2024-05-24 16:27:29 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.42 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -2,23 +2,18 @@ class Solution {
     public long solution(int w, int h) {
         long answer = 1;
         
-        // System.out.println(inc);
-        answer = (long)w*(long)h;
+        answer = (long)w*h;
         long count = 0;
         for(int i=0; i<w; i++){
             int leftX = i;
             int rightX = i+1;
-//             long leftY = leftX * h/w;
             
-//             long rightY = (rightX *h + w-1) / w;
-            
-            
-            long lY = (long)Math.floor((double)h*leftX/w);
-            long rY = (long)Math.ceil((double)h*rightX/w);
+            long leftY = (long)Math.floor((double)h*leftX/w);
+            long rightY = (long)Math.ceil((double)h*rightX/w);
             // System.out.print("[" + leftX + ", " + rightX + ") ");
             // System.out.println("[" + leftY + ", " + rightY + ")");
             
-            answer -= rY - lY;
+            answer -= rightY - leftY;
         }
 
         return answer;
