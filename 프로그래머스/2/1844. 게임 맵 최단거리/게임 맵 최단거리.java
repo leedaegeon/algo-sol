84e74f0b5beb91e5eff1625dd8540b6221403550
이대건 <ldg03198@gmail.com>
2024-07-02 17:08:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.46 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,47 @@
 import java.util.*;
 class Solution {
-    static int[] di = {-1, 0, 1, 0};
-    static int[] dj = {0, 1, 0, -1};
+    static int[] di = {0, 1, 0, -1};
+    static int[] dj = {1, 0, -1, 0};
     public int solution(int[][] maps) {
         int answer = 0;
-        int n = maps.length;
-        int m = maps[0].length;
-        Queue<int[]> q = new ArrayDeque<>();
-        int k=0;
-        q.offer(new int[]{0, 0});
+        int n = maps.length-1;
+        int m = maps[0].length-1;
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(0, 0));
+        int k=1;
         while(!q.isEmpty()){
             int size = q.size();
+            
             for(int i=0; i<size; i++){
-                int[] now = q.poll();
-                if(now[0] == n-1 && now[1] == m-1){
-                    answer = k;
-                    break;
-                }
-                if(maps[now[0]][now[1]] == 2){
-                    continue;
-                }
-                maps[now[0]][now[1]] = 2;
-                for(int dir=0; dir<4; dir++){
-                    int nexty = now[0] + di[dir];
-                    int nextx = now[1] + dj[dir];
-                    if(nexty < 0 ||nexty >= n || nextx < 0 || nextx >= m || maps[nexty][nextx] == 0 || maps[nexty][nextx] == 2){
-                        continue;
-                    }
-                    q.offer(new int[]{nexty, nextx});
+                Node now = q.poll();
+
+                if(now.y == n && now.x == m){
+                    return k;
                 }
                 
-            }
-            if(answer != 0){
-                break;
+                for(int d=0; d<4; d++){
+                    int nexty = now.y + di[d];
+                    int nextx = now.x + dj[d];
+                    if(nexty < 0 || nexty > n || nextx < 0 || nextx > m || maps[nexty][nextx] != 1){
+                        continue;
+                    }
+                    q.offer(new Node(nexty, nextx));
+                    maps[nexty][nextx] = -1;
+                }
             }
             k++;
-
         }
-        if(answer == 0){
-            answer = -1;
-        }else{
-            answer++;
+        return -1;
+    }
+    class Node{
+        int y;
+        int x;
+        Node(int y, int x){
+            this.y = y;
+            this.x = x;
         }
-        
-        return answer;
+        public String toString(){
+            return "y: " + y + " x: " + x;
+        }
     }
 }
