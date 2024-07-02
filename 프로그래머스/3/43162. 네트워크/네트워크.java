96ddbc88b2c4fafafbbf1e8222b06f8b725a2aee
이대건 <ldg03198@gmail.com>
2024-07-02 17:32:41 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,40 +1,23 @@
 import java.util.*;
 class Solution {
+    static Set<Integer> visited = new HashSet<>();
+    
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        
+        int answer = 0;    
         for(int i=0; i<computers.length; i++){
-            for(int j=0; j<computers.length; j++){
-                if(computers[i][j] == 1){
-                    computers = bfs(i, j, computers);
-                    answer++;
-                }
+            if(visited.add(i)){
+                dfs(i, computers);
+                answer++;
             }
         }
         return answer;
     }
-    public int[][] bfs(int y, int x, int[][] computers){
-        Queue<Node> q = new ArrayDeque<>();
-        q.offer(new Node(y,x));
-        while(!q.isEmpty()){
-            Node now = q.poll();
-            computers[now.y][now.x] = 0;
-            
-            for(int i=0; i<computers[now.x].length; i++){
-                if(computers[now.x][i] == 1){
-                    q.offer(new Node(now.x, i));
-                }
+    public void dfs(int nodeNum, int[][] computers){
+        
+        for(int i=0; i<computers[nodeNum].length; i++){
+            if(nodeNum != i && computers[nodeNum][i] == 1 && visited.add(i)){
+                dfs(i, computers);
             }
-            
-        }
-        return computers;
-    }
-    class Node{
-        int y;
-        int x;
-        Node(int y, int x){
-            this.y = y;
-            this.x = x;
         }
     }
 }
