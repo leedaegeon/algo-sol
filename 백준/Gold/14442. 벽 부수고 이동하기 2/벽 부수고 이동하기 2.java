142c3ab886e92f506e577dbaa490c381125cdcaf
이대건 <ldg03198@gmail.com>
2024-07-07 10:42:07 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1652 ms, Memory: 338892 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -12,14 +12,20 @@ public class Main {
         int k = Integer.parseInt(input[2]);
         int[][] field = new int[n][m];
         for(int i=0; i<n; i++){
-            input = br.readLine().split("");
-            for(int j=0; j<m; j++){
-                field[i][j] = Integer.parseInt(input[j]);
+            String inputStr = br.readLine();
+//            input = br.readLine().split("");
+//            for(int j=0; j<m; j++){
+//                field[i][j] = Integer.parseInt(input[j]);
+//            }
+            for(int j=0; j<inputStr.length(); j++){
+                field[i][j] = inputStr.charAt(j) - '0';
             }
         }
         boolean [][][] visited = new boolean[n][m][k+1];
         Queue<Node> q = new ArrayDeque<>();
         q.offer(new Node(0, 0, k, 1));
+        visited[0][0][k] = true;
+
         while(!q.isEmpty()){
             Node now = q.poll();
             if(now.y == n-1 && now.x == m-1){
@@ -36,7 +42,7 @@ public class Main {
                 if(field[nexty][nextx] == 1 && now.cnt>0 && !visited[nexty][nextx][now.cnt-1]){
                     q.offer(new Node(nexty, nextx, now.cnt-1, now.len+1));
                     visited[nexty][nextx][now.cnt-1] = true;
-                }else if(field[nexty][nextx] == 0 && now.cnt>=0 && !visited[nexty][nextx][now.cnt]){
+                }else if(field[nexty][nextx] == 0 && !visited[nexty][nextx][now.cnt]){
                     q.offer(new Node(nexty, nextx, now.cnt, now.len+1));
                     visited[nexty][nextx][now.cnt] = true;
                 }
