614a0245ad02137afb72f1aee129e8c0128a9070
이대건 <ldg03198@gmail.com>
2024-07-08 13:20:06 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,20 +1,16 @@
 -- 코드를 작성해주세요
+select (select sum(g2.score)
+        from hr_grade g2
+        group by g2.emp_no
+        order by sum(g2.score) desc
+        limit 1
+        ) as SCORE,
+        em.emp_no, em.emp_name, em.position, em.email
+from hr_employees em
+where em.emp_no = (select g2.emp_no
+                    from hr_grade g2
+                    group by g2.emp_no
+                     having sum(g2.score)
+                    order by sum(g2.score) desc
+                    limit 1)
 
-# select sum(g.score) as score, e.emp_no, e.emp_name, e.position, e.email
-# from HR_EMPLOYEES e
-# join HR_GRADE g
-# on e.emp_no = g.emp_no
-# where sum(g.score) = (select sum(score) 
-#                     from HR_GRADE 
-#                     group by EMP_no 
-#                     order by sum(score) DESC
-#                     limit 1)
-# group by g.emp_no
-
-select sum(g.score) as score, e.emp_no, e.emp_name, e.position, e.email
-from HR_EMPLOYEES e
-right join HR_GRADE g
-on e.emp_no = g.emp_no
-group by g.emp_no
-order by score DESC
-limit 1
