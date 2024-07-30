c96c27c784792f17b71f5994cdc037472f05d5d0
이대건 <ldg03198@gmail.com>
2024-07-30 23:27:52 +0900

    [Silver III] Title: 블로그, Time: 476 ms, Memory: 43152 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split("\\s+");
+        int n, x;
+        n = Integer.parseInt(input[0]);
+        x = Integer.parseInt(input[1]);
+        input = br.readLine().split("\\s+");
+        int[] income = new int[input.length];
+        for(int i=0; i<income.length; i++){
+            income[i] = Integer.parseInt(input[i]);
+        }
+        int left = 0;
+        int right = x;
+        int record = 0;
+        for(int i=0; i<x; i++){
+            record += income[i];
+        }
+        int cont = record==0?0:1;
+        int answer = record;
+        while(right < income.length){
+
+
+            if(record-income[left] <= record-income[left] + income[right]){
+                record = record -income[left] + income[right];
+
+                left++;
+                right++;
+                if(answer == record && answer != 0){
+                    cont++;
+                }else if(answer < record){
+                    cont = 1;
+                    answer = record;
+                }
+
+
+            } else {
+                left++;
+                right++;
+            }
+//            System.out.println(record);
+        }
+        if(record == 0){
+            System.out.println("SAD");
+        }else{
+            System.out.println(answer);
+            System.out.println(cont);
+        }
+
+    }
+}
