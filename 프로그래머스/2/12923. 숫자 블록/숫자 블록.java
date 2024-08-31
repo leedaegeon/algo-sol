11b36ceaa5bc1ba07f1b4f9c61946566d1de8c80
이대건 <ldg03198@gmail.com>
2024-08-31 16:34:30 +0900

    [level 2] Title: 숫자 블록, Time: 120.85 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public int[] solution(long begin, long end) {
+        
+        // List<Integer> answer = new ArrayList<>();
+        int[] answer = new int[(int)end - (int)begin+ 1];
+        int start = 0;
+        
+        for(int i=start; i<answer.length; i++){
+            int num = (int)begin+i;
+            int maxDivisor = getMaxDivisor(num);
+            answer[i] = maxDivisor;
+            
+        }
+        return answer;
+    }
+    private static int getMaxDivisor(int num) {
+        if (num == 1) {
+         return 0;
+        }
+
+        int possibleDivisor = -1;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                possibleDivisor = i;
+                if (num / i <= Math.pow(10,7)) {
+                   return num/i;
+                }
+            }
+        }
+
+        if(possibleDivisor != -1){
+            return possibleDivisor;
+        }
+
+        return 1;
+   }
+}
