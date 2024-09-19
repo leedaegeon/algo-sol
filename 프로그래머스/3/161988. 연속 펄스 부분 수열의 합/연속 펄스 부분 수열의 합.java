09bdd2393ef5761fa2b01b323d386b59aff86cba
이대건 <ldg03198@gmail.com>
2024-09-20 00:10:52 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 38.88 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Solution {
+    public long solution(int[] sequence) {
+        int[] posMulSequence = getMulSequence(1, sequence);
+        int[] negMulSequence = getMulSequence(0, sequence);
+        long pos = getSubSequenceSum(posMulSequence);
+        long neg = getSubSequenceSum(negMulSequence);
+        long answer = Math.max(pos, neg);
+
+        
+        return answer;
+    }
+
+    public long getSubSequenceSum(int[] sequence){
+        long[] cumSum = new long[sequence.length];
+        cumSum[0] = sequence[0];
+        long calcSum = cumSum[0];
+        long minSum = 0;
+        
+        for(int i=1; i<sequence.length; i++){
+            
+            cumSum[i] = cumSum[i-1] + sequence[i];
+            
+            // System.out.println(cumSum[i] + " - " + minSum +" "+ (cumSum[i] - minSum) +" vs " + calcSum);
+            
+            calcSum = Math.max(cumSum[i] - minSum, calcSum);
+            minSum = Math.min(minSum, cumSum[i-1]);
+            
+        }
+
+        // System.out.println(Arrays.toString(cumSum));
+        return calcSum;
+    }
+//     sign이 짝수면 펄스 수열 1부터 시작
+    public int[] getMulSequence(int sign, int[] sequence){
+        int [] newSequence = new int[sequence.length];
+        for(int i=0; i<sequence.length; i++){
+            if((i&1) == sign){
+                newSequence[i] = sequence[i] * -1;
+            }else{
+                newSequence[i] = sequence[i];
+            }
+        }
+
+        return newSequence;
+    }
+}
