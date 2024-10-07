fdb9c0c5fac4082b0af4f5ab1e58cfc10892843d
이대건 <ldg03198@gmail.com>
2024-10-07 17:30:23 +0900

    [Gold IV] Title: 뱀, Time: 108 ms, Memory: 14840 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -9,19 +9,16 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int k = Integer.parseInt(br.readLine());
-		int[][] map = new int[n][n];
-		
+		Set<Point> apple = new HashSet<>();
 		StringTokenizer st = null;
 		
 		for(int i=0; i<k; i++) {
 			st = new StringTokenizer(br.readLine());
 			int y = Integer.parseInt(st.nextToken());
 			int x = Integer.parseInt(st.nextToken());
-			map[y-1][x-1] = 9;
-			
+			apple.add(new Point(y-1, x-1));
 		}
 		int l = Integer.parseInt(br.readLine());
-		
 		Queue<Op> opQ = new ArrayDeque<>();
 		
 		for(int i=0; i<l; i++) {
@@ -29,13 +26,11 @@ public class Main {
 			opQ.add(new Op(Integer.parseInt(st.nextToken()), st.nextToken()));
 		}
 		
-		
-		
 		Deque<Point> snake = new LinkedList<>();
 		Set<Point> snakeSet = new HashSet<>();
 		snake.addFirst(new Point(0, 0, 1));
 		snakeSet.add(new Point(0, 0));
-		map[0][0] = 2;
+
 		int time = 0;
 	
 		while(true) {
@@ -53,47 +48,33 @@ public class Main {
 						dir = 3;
 					}
 				}
-				
 			}
 			
 			int nexty = head.y + dy[dir];
 			int nextx = head.x + dx[dir];
 			Point next = new Point(nexty, nextx, dir);
-			
-//			System.out.println("next: " + next);
-//			System.out.println(snakeSet.toString());
-			if(!checkWallAndBody(map, n, next, snakeSet)) {
+			if(!checkWallAndBody(n, next, snakeSet)) {
 				System.out.println(time+1);
 				break;
 			}
 			 
 			snake.addFirst(next);
 			snakeSet.add(next);
-			if(snake.size() > 1 && map[next.y][next.x] != 9) {
+			
+			if(snake.size() > 1 && !apple.contains(next)) {
 				Point tail = snake.pollLast();
 				snakeSet.remove(tail);
-				map[tail.y][tail.x] = 0; 
+			}else if(apple.contains(next)){
+				apple.remove(next);
 			}
-			map[next.y][next.x] = 2;
-//			System.out.println(snake.toString());
-//			for(int i=0; i<n; i++) {
-//				System.out.println(Arrays.toString(map[i]));
-//			}
-//			System.out.println("====================");
 			
 			time++;
 		}
-		
-//		for(int i=0; i<n; i++) {
-//			System.out.println(Arrays.toString(map[i]));
-//		}
-//		while(!opQ.isEmpty()) {
-//			System.out.println(opQ.poll());
-//		}
+	
 	}
 	
 	
-	public static boolean checkWallAndBody(int[][] map, int n, Point next, Set<Point> snakeSet) {
+	public static boolean checkWallAndBody(int n, Point next, Set<Point> snakeSet) {
 		
 		if(next.y < 0 || next.y >= n || next.x < 0 || next.x >= n || snakeSet.contains(next)) {
 			return false;
