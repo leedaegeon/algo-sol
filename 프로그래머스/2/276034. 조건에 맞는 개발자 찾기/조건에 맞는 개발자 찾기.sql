9bde2c9f0f9857a155802f0a231918d363d9c7c3
이대건 <ldg03198@gmail.com>
2024-10-18 22:51:10 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,15 +1,4 @@
--- 코드를 작성해주세요
-select dev.id, dev.email, dev.first_name, dev.last_name
-from developers dev 
-# join skillcodes skill
-# on dev.skill_code = skill.code
-where dev.skill_code & (select sum(sk.code)
-                        from skillcodes sk
-                        where sk.name = "Python" or sk.name = "C#"
-                        group by sk.category) != 0
-order by 1 asc
-
-# select sum(sk.code) & 400, sum(sk.code) & 2048, sum(sk.code) & 1024, sum(sk.code) & 16384, sum(sk.code) & 8452
-# from skillcodes sk
-# where sk.name = "Python" or sk.name = "C#"
-# group by sk.category
+select d.id, d.email, d.first_name, d.last_name
+from developers d
+where d.skill_code & (select sum(s.code) from skillcodes s where name in('python', 'c#')) != 0
+order by 1 ASC
