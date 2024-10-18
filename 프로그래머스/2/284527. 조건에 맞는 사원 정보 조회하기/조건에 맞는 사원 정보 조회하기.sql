de2b3c243ff92fa06cb06a54c5593386fe60d901
이대건 <ldg03198@gmail.com>
2024-10-18 23:01:24 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,9 +1,14 @@
-select sum(g.score) as score, em.emp_no, em.emp_name,  em.position, em.email
-from HR_EMPLOYEES em join HR_GRADE g
-on em.emp_no = g.emp_no
-group by em.emp_no
-having sum(g.score) = (select sum(g2.score) 
-                       from HR_GRADE g2
-                      group by g2.emp_no
-                      order by 1 desc
-                      limit 1)
+select (select sum(g.score)
+                from hr_grade g 
+                where g.year = 2022 
+                group by g.emp_no
+                order by sum(g.score) DESC
+                limit 1) as score, e.emp_no, e.emp_name, e.position, e.email
+from hr_employees e
+where e.emp_no = (select g.emp_no
+                from hr_grade g 
+                where g.year = 2022 
+                group by g.emp_no
+                order by sum(g.score) DESC
+                limit 1)
+        
