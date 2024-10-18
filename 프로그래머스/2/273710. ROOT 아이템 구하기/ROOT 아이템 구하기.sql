04021d6127e73ce5f7df7ed6f1321f473218325c
이대건 <ldg03198@gmail.com>
2024-10-18 21:59:33 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,6 +1,4 @@
--- 코드를 작성해주세요
-select i.item_id, i.item_name
-from item_info i
-join item_tree t
-on i.item_id = t.item_id
-where t.PARENT_ITEM_ID is null
+select t.item_id, i.item_name
+from item_tree t join item_info i on t.item_id = i.item_id
+where t.parent_item_id is null
+order by 1 ASC
